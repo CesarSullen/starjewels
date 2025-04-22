780acf024cd018a3afe300466b571aaaab1dd2e3
Cesar <cesarsuarez2303@gmail.com>
2025-04-22 14:59:54 +0200

    Added returns policy doc

--- js/products.js
+++ js/products.js
@@ -79,6 +79,9 @@ document.addEventListener("DOMContentLoaded", () => {
 		  <a href="../docs/how-to.pdf">
 			<button class="link">¿Cómo cuidar tus joyas?</button>
 		  </a>
+		  <a href="../docs/returns-policy.html">
+					<button class="link">Política de devoluciones</button>
+		  </a>
 		  <button class="btn">Comprar</button>
 		</div>
 	  `;
